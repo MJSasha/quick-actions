6a0ce2f7345e612581190f75780036ddcd682c8d
MJSasha <sasha.ushankov@gmail.com>
2023-07-02 10:04:24 +0300

    Add include sample

--- Sample.Api/AppDbContext.cs
+++ Sample.Api/AppDbContext.cs
@@ -13,12 +13,18 @@ namespace Sample.Api
         {
             modelBuilder.Entity<User>().HasKey(x => x.Id);
 
+            modelBuilder.Entity<Role>().HasData
+            (
+                new Role { Id = 1, Name = "Admin" },
+                new Role { Id = 2, Name = "User" }
+            );
+
             modelBuilder.Entity<User>().HasData
             (
-                new User { Id = 1, Name = "Andriy", Email = "Andriy@mail", Phone = "123" },
-                new User { Id = 2, Name = "Sam", Email = "Sam@mail", Phone = "456" },
-                new User { Id = 3, Name = "Sasha", Email = "Sasha@mail", Phone = "789" },
-                new User { Id = 4, Name = "Nick", Email = "Nick@mail", Phone = "791" }
+                new User { Id = 1, Name = "Andriy", Email = "Andriy@mail", Phone = "123", RoleId = 1 },
+                new User { Id = 2, Name = "Sam", Email = "Sam@mail", Phone = "456", RoleId = 2 },
+                new User { Id = 3, Name = "Sasha", Email = "Sasha@mail", Phone = "789", RoleId = 2 },
+                new User { Id = 4, Name = "Nick", Email = "Nick@mail", Phone = "791", RoleId = 2 }
             );
         }
     }
---
+++ Sample.Api/Migrations/20230702070224_AddedRoles.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Sample.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RoleId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Admin" },
+                    { 2, "User" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "RoleId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "RoleId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "RoleId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "RoleId",
+                value: 2);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Role_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "Role",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Role_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Role");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "Users");
+        }
+    }
+}
